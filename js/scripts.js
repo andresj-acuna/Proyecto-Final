cbfbd18648083d731c629579222c431c07fa0b01
Andres Acuña <andres.cbaa@gmail.com>
2021-02-19 06:28:55 -0300

    Se agrega el archivo scripts.js y se modifica el index

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+(function($){
+    var typed = new Typed ('span.text-rotate',{
+   	strings: ["Soy Desarrollador Web", "Soy Desarrollador Worpress"],
+   	typeSpeed: 100, 
+   	backSpeed: 100,
+   	fadeOut: false,
+   	smartBackspace: true,
+   	loop: true
+   });
+})(jQuery);
